6e79d6dd63d9fb529ea4b5e0e94e9644114537a6
PaulZC <pc235603@gmail.com>
2021-06-11 14:01:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# SparkFun STC3x CO<sub>2</sub> Sensor Library
+
+[![SparkX STC31 Breakout (Qwiic) (SPX-nnnnn)]()](https://www.sparkfun.com/products/nnnnn)
+
+[*SparkX STC31 Breakout (Qwiic) (SPX-nnnnn)*](https://www.sparkfun.com/products/nnnnn)
+
+An Arduino library for the STC3x family of CO<sub>2</sub> sensors from Sensirion. The STC31 uses Sensirion’s patented CMOSens® sensor technology to measure CO<sub>2</sub> concentrations up to 100%.
+
+We've written this Arduino library to make reading the CO<sub>2</sub> concentration very easy. It can be downloaded through the Arduino Library manager: search for 'SparkFun STC3x'.
+
+Library written by Paul Clark, based on the SCD30 library by Nathan Seidle ([SparkFun](http://www.sparkfun.com)).
+
+Repository Contents
+-------------------
+
+* **/examples** - Example sketches for the library (.ino). Run these from the Arduino IDE.
+* **/src** - Source files for the library (.cpp, .h).
+* **keywords.txt** - Keywords from this library that will be highlighted in the Arduino IDE.
+* **library.properties** - General library properties for the Arduino package manager.
+
+Documentation
+--------------
+
+* **[Installing an Arduino Library Guide](https://learn.sparkfun.com/tutorials/installing-an-arduino-library)** - Basic information on how to install an Arduino library.
+
+License Information
+-------------------
+
+This product is _**open source**_!
+
+Please use, reuse, and modify these files as you see fit. Please maintain attribution to SparkFun Electronics and release anything derivative under the same license.
+
+Distributed as-is; no warranty is given.
+
+- Your friends at SparkFun.
---
+++ examples/Example1_BasicReadings/Example1_BasicReadings.ino
@@ -0,0 +1,74 @@
+/*
+  Reading CO2 and temperature from the STC3x
+  By: Paul Clark
+  Based on earlier code by: Nathan Seidle
+  SparkFun Electronics
+  Date: June 11th, 2021
+  License: MIT. See license file for more information but you can
+  basically do whatever you want with this code.
+
+  Feel like supporting open source hardware?
+  Buy a board from SparkFun! https://www.sparkfun.com/products/nnnnn
+
+  This example prints the current CO2 concentration (%) and temperature in C.
+
+  Hardware Connections:
+  Attach RedBoard to computer using a USB cable.
+  Connect STC31 to RedBoard using Qwiic cable.
+  Open Serial Monitor at 115200 baud.
+*/
+
+#include <Wire.h>
+
+#include "SparkFun_STC3x_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_STC3x
+STC3x mySensor;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(F("STC3x Example"));
+  Wire.begin();
+
+  mySensor.enableDebugging(); // Uncomment this line to get helpful debug messages on Serial
+
+  if (mySensor.begin() == false)
+  {
+    Serial.println(F("Sensor not detected. Please check wiring. Freezing..."));
+    while (1)
+      ;
+  }
+
+  //We need to tell the sensor what binary gas and full range we are using
+  //Possible values are:
+  //  STC3X_BINARY_GAS_CO2_N2_100   : Set binary gas to CO2 in N2.  Range: 0 to 100 vol%
+  //  STC3X_BINARY_GAS_CO2_AIR_100  : Set binary gas to CO2 in Air. Range: 0 to 100 vol%
+  //  STC3X_BINARY_GAS_CO2_N2_25    : Set binary gas to CO2 in N2.  Range: 0 to 25 vol%
+  //  STC3X_BINARY_GAS_CO2_AIR_25   : Set binary gas to CO2 in Air. Range: 0 to 25 vol%
+
+  if (mySensor.setBinaryGas(STC3X_BINARY_GAS_CO2_AIR_25) == false)
+  {
+    Serial.println(F("Could not set the binary gas! Freezing..."));
+    while (1)
+      ;
+  }
+}
+
+void loop()
+{
+  if (mySensor.measureGasConcentration()) // measureGasConcentration will return true when fresh data is available
+  {
+    Serial.println();
+
+    Serial.print(F("CO2(%):"));
+    Serial.print(mySensor.getCO2(), 2);
+
+    Serial.print(F("\tTemperature(C):"));
+    Serial.print(mySensor.getTemperature(), 2);
+
+    Serial.println();
+  }
+  else
+    Serial.print(F("."));
+
+  delay(100);
+}
